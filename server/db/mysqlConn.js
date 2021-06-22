000ba46b54bb5e0e76215042645974a50d7c4b5c
Pallab-Roy <79366238+Pallab-Roy@users.noreply.github.com>
2021-06-22 23:55:38 +0530

    secondary

--- server/db/mysqlConn.js
+++ server/db/mysqlConn.js
@@ -2,7 +2,7 @@ var mysql = require('mysql')
 var MsqConn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password',
+  password: '',
   database: 'quora',
   multipleStatements: true
 });
